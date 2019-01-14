8cb0f64ab2839c83e9c44b163a87ab1dfe6f6280
Denniswzm <denniswzm@gmail.com>
2019-01-14 16:13:04 -0500

    initial commit

---
+++ lab 0/main/java/Hello.java
@@ -0,0 +1,12 @@
+import ca.roumani.i2c.Utility;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println(Math.max(3, 5));
+        System.out.println(Utility.gcd(24, 18));
+        System.out.println(Math.pow(2, 3));
+        System.out.println("??");
+        System.out.println(Utility.factorial(3));
+        System.out.println(Utility.gf(104));
+    }
+}
---
+++ lab 0/main/java/com/example/dennis97/zero/ZeroActivity.java
@@ -0,0 +1,14 @@
+package com.example.dennis97.zero;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ZeroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zero);
+    }
+
+}
